e042faf24493b012080ffbaa9b8a92e84a18f596
pramehra <prashast.mehra@finn.no>
2023-10-03 13:58:26 +0200

    Remove eik from vue as well

--- vue.eik.js
+++ vue.eik.js
@@ -1,10 +1,6 @@
-import * as eik from '@eik/esbuild-plugin';
 import esbuild from 'esbuild';
 
-await eik.load();
-
 await esbuild.build({
-  plugins: [eik.plugin()],
   entryPoints: ['vue/index.js'],
   bundle: true,
   outfile: 'dist/vue/icons.js',
