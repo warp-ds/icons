df471da1cfbf03bc891aa42aa6175206623eca67
pramehra <prashast.mehra@finn.no>
2023-10-03 09:50:21 +0200

    Fix bundling

--- scripts/build.js
+++ scripts/build.js
@@ -14,7 +14,7 @@ const descriptionsFile = readFileSync(path.join(basedir, 'icon-descriptions.yml'
 const descriptions = yaml.load(descriptionsFile)
 
 const SRC_DIR = path.join(basedir, "src/raw");
-const DIST_DIR = path.join(basedir, "dist");
+const DIST_DIR = path.join(basedir, "dist/icons");
 
 // these are the only colors we will replace to currentColor
 const magicColors = ['#71717a', '#767676', '#0063fb', '#d91f0a', '#d5840b', '#059e6f', '#0386bf', '#6f7d90'].map(v => v.toLowerCase())
