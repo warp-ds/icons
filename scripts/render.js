6d3f00703fac7ffeb010f4bfd5ea6eade2d27c48
Magnus Hübner <magnuh@users.noreply.github.com>
2024-07-15 12:14:09 +0200

    chore: Tweak icons preview page and update to use Warp css 2.0 (#123)

--- scripts/render.js
+++ scripts/render.js
@@ -1,25 +1,25 @@
 import fs from "fs-extra";
 import { glob } from "glob";
-import path from "path";
 import { getNameAndSize } from "./util/helpers.js";
-
-const svgPaths = glob.sync(`./dist/**/*.svg`);
 
 export const iconsBySize = () => {
   const iconsBySize = {};
+  const svgPaths = glob.sync(`./dist/**/*.svg`).sort();
   const icons = svgPaths.map((svgPath) => {
-    const data = fs.readFileSync(svgPath, "utf-8");
-    const {size} =  getNameAndSize(svgPath);
+    const svg = fs.readFileSync(svgPath, "utf-8");
+    const { name, size } = getNameAndSize(svgPath);
     return {
-      name: path.basename(path.dirname(svgPath)),
-      svg: data,
+      name,
+      svg,
       iconSize: size,
     };
-  });  
-  for (const icon of icons) {
-    const array = iconsBySize[icon.iconSize] || [];
-    array.push(icon);
-    iconsBySize[icon.iconSize] = array;
-  }
-  return iconsBySize;  
+  });
+  icons.forEach((icon) => {
+    if (iconsBySize[icon.iconSize]) {
+      iconsBySize[icon.iconSize].push(icon);
+    } else {
+      iconsBySize[icon.iconSize] = [icon];
+    }
+  });
+  return iconsBySize;
 };
